4ee0cc4b04ffb93c8d3d5c49f23ef3ce062b6e71
ul paidi <ulfaidi2002@gmail.com>
2024-04-30 22:30:38 +0700

    first commit

---
+++ practice.py
@@ -0,0 +1,9 @@
+import requests
+
+api_key = 'a9bf6621-087e-4249-8114-4ddd49dc8bb3'
+word = 'voluminous'
+root_url = 'https://www.dictionaryapi.com/api/v3/references/collegiate/json'
+final_url = f'{root_url}/{word}?key={api_key}'
+r = requests.get(final_url)
+result = r.json()
+print(result)
